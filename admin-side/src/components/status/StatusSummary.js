9c6d43accd1657c1107dafff4dbf7afd997cea9c
Namit Chandwani <namitchandwani99@gmail.com>
2020-06-21 03:04:13 +0530

    Fix values in table

--- admin-side/src/components/status/StatusSummary.js
+++ admin-side/src/components/status/StatusSummary.js
@@ -6,10 +6,11 @@ const StatusSummary = () => {
       <td>3/29/2020</td>
       <td>12</td>
       <td>379</td>
-      <td>No</td>
+      <td>5427</td>
       <td>8</td>
       <td>Yes</td>
-      <td>1,020</td>
+      <td>No</td>
+      <td>Yes</td>
       <td>Yes</td>
       <td>Yes</td>
     </tr>
