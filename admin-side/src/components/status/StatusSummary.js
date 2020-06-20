faf3975427fab2cc8949f1ba0386bea0da3b470c
Namit Chandwani <namitchandwani99@gmail.com>
2020-06-20 21:06:23 +0530

    Initialize StatusSummary component

---
+++ admin-side/src/components/status/StatusSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const StatusSummary = () => {
+  return (
+    <tr>
+      <td>3/29/2020</td>
+      <td>12</td>
+      <td>379</td>
+      <td>No</td>
+      <td>8</td>
+      <td>Yes</td>
+      <td>1,020</td>
+      <td>Yes</td>
+      <td>Yes</td>
+    </tr>
+  );
+};
+
+export default StatusSummary;
