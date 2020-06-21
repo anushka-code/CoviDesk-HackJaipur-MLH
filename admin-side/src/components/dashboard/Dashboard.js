bc8e762845ef8d2eae71a89edde5a480dac837cf
Namit Chandwani <namitchandwani99@gmail.com>
2020-06-21 13:32:46 +0530

    Connect to firestore and redux

--- admin-side/src/components/dashboard/Dashboard.js
+++ admin-side/src/components/dashboard/Dashboard.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 import StatusList from "../status/StatusList";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 class Dashboard extends Component {
   render() {
+    const { statuses } = this.props;
+
     return (
       <div className="dashboard container">
-        <StatusList />
+        <StatusList statuses={statuses} />
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  console.log(state);
+
+  return {
+    statuses: state.firestore.ordered.statuses,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "statuses" }])
+)(Dashboard);
